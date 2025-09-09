ee9b04cf10d3af22c696977ea92bc80496da9c25
Tom Vuma <vumatom10@gmail.com>
2025-09-09 22:18:14 +0200

    Update ProfilesList.js

--- client/src/components/profile/ProfilesList.js
+++ client/src/components/profile/ProfilesList.js
@@ -106,7 +106,7 @@ const ProfilesList = () => {
               <img
               src={
                 profile.profilePicture
-                  ? `${process.env.REACT_APP_API_URL.replace('/api', '')}${profile.profilePicture}`
+                  ? `${process.env.REACT_APP_API_URL.replace('/api', '')}${user.profilePicture}`
                   : "https://cdn-icons-png.flaticon.com/512/847/847969.png"
               }
               alt="Profile"
